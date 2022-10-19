fc21a4989c767b7de23238db86ff327f5a0aa02b
Wiktor Bandura <wiktor@MBP-Wiktor.home>
2022-10-19 13:50:39 +0200

    First time using styled components

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,3 +1,11 @@
-const App = () => <h1>Hello Wiktor</h1>;
+import Button from '../../components/Button/Button';
+
+const App = () => (
+  <>
+    <h1>Hello Wiktor</h1>
+    <Button>Close/Save</Button>
+    <Button secondary>Remove</Button>
+  </>
+);
 
 export default App;
