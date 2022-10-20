203ba023ff6a2a1b08eca7c9c8d0614198d163da
Wiktor Bandura <wiktor@MBP-Wiktor.home>
2022-10-20 10:42:09 +0200

    Storybook installation

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,9 @@
+import Button from '../components/Button/Button';
+
+export default {
+  title: 'Button',
+  compoment: Button,
+};
+
+export const Primary = () => <Button>Hello Wiktor</Button>;
+export const Secondary = () => <Button secondary>Hello</Button>;
